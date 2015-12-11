9e127dedbd5366bc4ff11f4949b1d8787e0c904a
Ali Yeysides <ali.yeysides@gmail.com>
2015-12-11 14:43:34 -0500

    Update utility.js

--- utility.js
+++ utility.js
@@ -1,16 +1,16 @@
 // Some useful functions and snippets
 
-                  // returns elements in that are in a but not in b
-                  // subtractAsSets([4, 5, 6], [4, 5, 7]) => [6]
-                  var subtractAsSets = function(a, b) {
-                      var result = [], inB = {}, i, n;
-                      for (i = 0, n = b.length; i < n; i++) {
-                          inB[b[i]] = true;
-                      }
-                      for (i = 0, n = a.length; i < n; i++) {
-                          if (!inB[a[i]]) {
-                              result.push(a[i]);
-                          }
-                      }
-                      return result;
-                  };
+  // returns elements in that are in a but not in b
+  // subtractAsSets([4, 5, 6], [4, 5, 7]) => [6]
+  var subtractAsSets = function(a, b) {
+      var result = [], inB = {}, i, n;
+      for (i = 0, n = b.length; i < n; i++) {
+          inB[b[i]] = true;
+      }
+      for (i = 0, n = a.length; i < n; i++) {
+          if (!inB[a[i]]) {
+              result.push(a[i]);
+          }
+      }
+      return result;
+  };
